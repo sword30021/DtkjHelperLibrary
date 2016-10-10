eb97d2ea97f9894399abdbf9383ad4634a6f6114
sword30021 <sword30021@gmail.com>
2016-10-10 14:13:36 +0800

    升级版本

--- SimpleDemo/src/main/java/com/dtkj/activities/MainActivity.java
+++ SimpleDemo/src/main/java/com/dtkj/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.dtkj;
+package com.dtkj.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dtkj.R;
 
 public class MainActivity extends AppCompatActivity {
 
